2dad286ee6d8c21e24d8c2aa74ceca546cdbc59d
atom497 <tomboudaud0105@gmail.com>
2026-02-04 09:31:40 +0100

    Initial commit

---
+++ src/main/java/com/eseo/mediastock/Launcher.java
@@ -0,0 +1,9 @@
+package com.eseo.mediastock;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
