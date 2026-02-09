b4ee54dcf14e10928d79071d20287814449139c1
remig <remi.goubin@outlook.com>
2026-02-09 09:41:45 +0100

    Connection Base de donnée

--- src/main/java/com/eseo/mediastock/dao/DatabaseConnection.java
+++ src/main/java/com/eseo/mediastock/dao/DatabaseConnection.java
@@ -1,4 +1,18 @@
 package com.eseo.mediastock.dao;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    static Dotenv dotenv = Dotenv.load();
+
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASS = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
