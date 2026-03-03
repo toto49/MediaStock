9e0c6c4526853d74153731067898e6cdfb4f04dd
Oxilian37 <lilianjautrou@gmail.com>
2026-03-03 13:47:24 +0100

    Corrections EmpruntDAO + Ajout methode trouverREtards

--- src/main/java/com/eseo/mediastock/dao/EmpruntDAO.java
+++ src/main/java/com/eseo/mediastock/dao/EmpruntDAO.java
@@ -2,6 +2,7 @@ package com.eseo.mediastock.dao;
 
 import com.eseo.mediastock.model.Adherent;
 import com.eseo.mediastock.model.Emprunt;
+import com.eseo.mediastock.model.Enum.EnumDispo;
 import com.eseo.mediastock.model.Exemplaire;
 
 import java.sql.Connection;
@@ -13,7 +14,7 @@ import java.time.LocalDate;
 public class EmpruntDAO {
 
     public void createEmprunt(Adherent adherent, Exemplaire exemplaire) throws SQLException {
-        String sql = "INSERT INTO EMPRUNT (date_debut, date_retour, id_adherent, id_exemplaire) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO EMPRUNT (date_debut, date_retour, id_adherent, id_exemplaire,statut) VALUES (?, ?, ? , ?, ?)";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -26,21 +27,40 @@ public class EmpruntDAO {
             stmt.setString(3, adherent.getId());
             // 4. id_exemplaire : INT(11) dans la BDD, on récupère l'ID de l'objet Exemplaire
             stmt.setInt(4, exemplaire.getId());
+            // 5. statut : On définit le statut à EMPRUNTE
+            stmt.setString(5, EnumDispo.EMPRUNTE.name());
 
             stmt.executeUpdate();
         }
     }
 
     public void saveRetour(Emprunt emprunt) throws SQLException {
-        String sql = "UPDATE EMPRUNT SET date_retour = ? WHERE id = ?"; // Ajouter status rendu
+        String sql = "UPDATE EMPRUNT SET date_retour = ?, status = ? WHERE id = ?";
 
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-            // 1. Date du jour = date de rendu réel
-            stmt.setDate(1, Date.valueOf(LocalDate.now()));
-            // 2. ID de l'emprunt (clé primaire)
-            stmt.setInt(2, emprunt.getId());
+            // 1. Date du jour (Conversion LocalDate vers java.sql.Date)
+            stmt.setDate(1, java.sql.Date.valueOf(LocalDate.now()));
+            // 2. Status : On utilise .name() pour enregistrer le String de l'Enum en base
+            stmt.setString(2, EnumDispo.RENDU.name());
+            // 3. ID de l'emprunt
+            stmt.setInt(3, emprunt.getId());
+
+            stmt.executeUpdate();
+        }
+    }
+
+    public void trouverRetards(LocalDate date) throws  SQLException {
+        String sql = "SELECT * FROM emprunt WHERE date_retour < ? AND statut = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            // 1. Date du jour (Conversion LocalDate vers java.sql.Date)
+            stmt.setDate(1, java.sql.Date.valueOf(LocalDate.now()));
+            // 2. Status : On utilise .name() pour enregistrer le String de l'Enum en base
+            stmt.setString(2, EnumDispo.RENDU.name());
 
             stmt.executeUpdate();
         }
