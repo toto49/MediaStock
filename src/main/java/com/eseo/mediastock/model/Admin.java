cced6a31d52fe84a01d13480aad21e6611988006
remig <remi.goubin@outlook.com>
2026-03-11 15:33:20 +0100

    Hash mdp création Admin

--- src/main/java/com/eseo/mediastock/model/Admin.java
+++ src/main/java/com/eseo/mediastock/model/Admin.java
@@ -4,29 +4,44 @@ public class Admin {
     private int id;
     private String email;
     private String passwordHash;
+    private String plainPassword;
 
     public Admin(int id, String email, String passwordHash) {
         this.id = id;
         this.email = email;
         this.passwordHash = passwordHash;
+
     }
 
     public int getId() {return id;}
     public void setId(int id) {this.id = id;}
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getPasswordHash() {
+
         return passwordHash;
     }
 
     public void setPasswordHash(String passwordHash) {
+
         this.passwordHash = passwordHash;
+    }
+
+    public String getPlainPassword() {
+
+        return plainPassword;
+    }
+
+    public void setPlainPassword(String plainPassword) {
+        this.plainPassword = plainPassword;
     }
 }
