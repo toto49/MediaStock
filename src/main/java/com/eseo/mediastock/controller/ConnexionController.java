d6103fc648e0c09964c9833eddc5056c8bf5fb8f
toto49 <tomboudaud0105@gmail.com>
2026-03-11 14:28:23 +0100

    feat : ajout d'un header personnalisé de l'application.
    Signed-off-by: toto49 <tomboudaud0105@gmail.com>

--- src/main/java/com/eseo/mediastock/controller/ConnexionController.java
+++ src/main/java/com/eseo/mediastock/controller/ConnexionController.java
@@ -1,11 +1,10 @@
 package com.eseo.mediastock.controller;
 
+import com.eseo.mediastock.HelloApplication;
 import com.eseo.mediastock.Launcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.PasswordField;
@@ -25,9 +24,8 @@ public class ConnexionController {
         FXMLLoader loader = new FXMLLoader(
                 Launcher.class.getResource("view/bienvenue-view.fxml"));
         Parent root = loader.load();
-        Scene currentScene = ((Node) actionEvent.getSource()).getScene();
-        currentScene.setRoot(root);
 
+        HelloApplication.changerPageGlobale(root, "Bienvenue");
     }
 
     public void Buttonsend(ActionEvent actionEvent) throws IOException {
@@ -43,16 +41,10 @@ public class ConnexionController {
             stage.getScene().setFill(Color.TRANSPARENT);
             alert.showAndWait();
 
-
         } else {
             FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("view/menu-view.fxml"));
             Parent root = fxmlLoader.load();
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setTitle("Menu");
-            Scene currentScene = stage.getScene();
-            currentScene.setRoot(root);
-
+            HelloApplication.changerPageGlobale(root, "Menu Principal");
         }
-
     }
 }
