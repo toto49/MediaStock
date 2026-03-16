d1acb1c83ad0318471e79ce2533364d4b608d862
remig <remi.goubin@outlook.com>
2026-03-16 14:31:22 +0100

    Refractor : BCrypt Spring vers Bcrypt simple

--- src/main/java/com/eseo/mediastock/service/PasswordUtilService.java
+++ src/main/java/com/eseo/mediastock/service/PasswordUtilService.java
@@ -1,18 +1,16 @@
 package com.eseo.mediastock.service;
 
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordUtilService {
 
-    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
     public static String hash(String password) {
-        return encoder.encode(password);
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
 
     public static boolean verify(String hash, String password) {
-        return encoder.matches(password, hash);
+        return BCrypt.checkpw(password, hash);
     }
 }
+
 
